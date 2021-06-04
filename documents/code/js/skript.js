80f46b255f7606d2e85ee2e118d5ea6335d4f9df
Alla <alla.shterndok@gmail.com>
2021-06-04 13:00:32 +0300

    second

---
+++ documents/code/js/skript.js
@@ -0,0 +1,17 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm("are you hear?");
+// console.log(result);
+
+// const answer = +prompt("are you 18?", "18");
+// console.log(answer +5);
+
+const answers = [];
+
+answers [0] = prompt('как ваше имя', '')
+answers [1] = prompt('как ваша фамилия', '')
+answers [2] = prompt('сколько вам лет', '')
+
+document.write(answers);
